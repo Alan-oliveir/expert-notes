b05392d5b5f5a15a8a01169ecd103c6640b6368b
Alan de Oliveira Gonçalves <alandeog@poli.ufrj.br>
2024-02-10 23:51:41 -0300

    Add NoteCard component to app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,7 @@
 import logo from "./assets/logo-nlw-expert.svg";
 
+import { NoteCard } from "./components/note-card";
+
 export function App() {
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -16,7 +18,12 @@ export function App() {
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <div className=""></div>
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
       </div>
     </div>
   );
